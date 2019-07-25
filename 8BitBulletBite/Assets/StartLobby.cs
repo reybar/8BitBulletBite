8325632fa5d7b73708bc4b4700d35074c48a1d75
mrabar <marinrabar@gmail.com>
2019-07-25 18:14:30 +0200

    splash screen added

---
+++ 8BitBulletBite/Assets/StartLobby.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartLobby : MonoBehaviour
+{
+    public void LoadLobbyScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
